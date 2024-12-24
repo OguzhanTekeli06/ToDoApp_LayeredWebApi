2694d4c420fa2f66a0814b039e342554ce0b32e2
Oğuzhan Tekelioğlu <130230042+OguzhanTekeli06@users.noreply.github.com>
2024-12-24 15:56:48 +0300

    gitignore

--- ToDoListApp/obj/Debug/net8.0/ToDoListApp.AssemblyInfo.cs
+++ ToDoListApp/obj/Debug/net8.0/ToDoListApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoListApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6bcaea599cffd6e5c5d37ce5ceaa464e5cd9e0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53ee8686131fd949fdba0bf2dd342e2254f54e9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoListApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoListApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
